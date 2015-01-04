0f1dad9683bba6a92d4738d61fe77fff5512e3a2
Serhioromano <serg4172@mail.ru>
2015-01-05 03:26:57 +0800

    Good. It works.

--- angular-s3.js
+++ angular-s3.js
@@ -1,10 +1,11 @@
 (!function() {
 	angular.module('angular.aws.s3', []).
 
-		directive('s3Input', function() {
+		directive('s3Input', function($filter) {
 			return {
 				restrict: 'AE',
-				link:     function(scope, el, attrs) {
+				require: '^ngModel',
+				link:     function(scope, el, attrs, ngModel) {
 					if(scope.options.multiple) {
 						el.attr('multiple', 'true');
 					}
@@ -15,17 +16,23 @@
 							if(angular.isArray(scope.options.extensions) && scope.options.extensions.length > 0) {
 								var ext = v.name.split('.').pop().toLowerCase();
 								if(scope.options.extensions.indexOf(ext) == -1) {
-									alert('extension');
+									if(angular.isFunction(scope.options.cb_error)) {
+										scope.options.cb_error('Unhallowed file extension: '+ ext);
+									}
 									return true;
 								}
 							}
 							if(scope.options.filesize > 0 && scope.options.filesize < v.size) {
-								alert('size');
+								if(angular.isFunction(scope.options.cb_error)) {
+									scope.options.cb_error('File size bigger than allowed: ' + $filter('filesize')(scope.options.filesize));
+								}
 								return true;
 							}
 							if(scope.options.limit > 0 && scope.files.length >= scope.options.limit) {
 								// todo replace file.
-								alert('limit');
+								if(angular.isFunction(scope.options.cb_error)) {
+									scope.options.cb_error('You have reached maximum amount of files to upload: ' + scope.options.limit);
+								}
 								return;
 							}
 
@@ -52,25 +59,38 @@
 		}).
 
 		directive('s3', function(S3UploaderSrv) {
-			function link(scope, element, attrs) {
+			function link(scope, element, attrs, ngModel) {
+
+				ngModel.$render = function () {
+					scope.files = ngModel.$viewValue;
+				};
+
+				ngModel.$setValidity('upload', false);
+				ngModel.$render();
 
 				scope.upload = function() {
 					scope.start_upload_state = true;
 					if(scope.options.policyUrl) {
 						$http.get(scope.options.policyUrl).success(function(response, status) {
 							if(!angular.isObject(response)) {
-								alert('wrong policy');
+								if(angular.isFunction(scope.options.cb_error)) {
+									scope.options.cb_error('Service returned not a polilcy');
+								}
 								return;
 							}
 							scope.options.policy = response;
 							_upload();
 						}).error(function(error, status) {
-							alert('get no policy');
+							if(angular.isFunction(scope.options.cb_error)) {
+								scope.options.cb_error('Could not get a policy');
+							}
 						});
 					} else if(angular.isObject(scope.options.policy)) {
 						_upload();
 					} else {
-						alert('no policy');
+						if(angular.isFunction(scope.options.cb_error)) {
+							scope.options.cb_error('No policy parameter');
+						}
 					}
 				};
 
@@ -85,9 +105,9 @@
 				restrict:    'EA',
 				templateUrl: 'uploader.html',
 				link:        link,
+				require:     'ngModel',
 				scope:       {
-					options: '=',
-					files:   '='
+					options: '='
 				}
 			}
 		}).
@@ -130,7 +150,7 @@
 				file.progress = 1;
 
 				function uploadProgress(e) {
-					scope.$apply(function(){
+					scope.$apply(function() {
 						if(e.lengthComputable) {
 							file.progress = Math.round(e.loaded * 100 / e.total);
 						}
